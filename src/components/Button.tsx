77063e4ef20ecc9a8dee588b4dcc34bf7d0ff753
Rafael Angonese <rafael.angonese09@gmail.com>
2021-06-26 15:50:50 -0300

    button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import styles from '../styles/button.module.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+  return (
+    <button className={styles.button} {...props} />
+  )
+}
